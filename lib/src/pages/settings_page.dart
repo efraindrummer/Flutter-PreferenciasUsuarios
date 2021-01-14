5550a3d9439a63d5df1c9d243293a0fd39772ea8
Efrain May <efrain_may_75@hotmail.com>
2021-01-14 02:03:13 -0600

    shared preferences

--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:preferenciasusuarioapp/src/widgets/menu_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsPage extends StatefulWidget {
 
@@ -18,9 +19,27 @@ class _SettingsPageState extends State<SettingsPage> {
   TextEditingController _textController;
 
   @override
-  void initState() {
+  void initState(){
     super.initState();
+    cargarPref();
     _textController = new TextEditingController(text: _nombre);
+  }
+
+  cargarPref() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    _genero = prefs.getInt('genero');
+    
+    setState(() {});
+  }
+
+  _setSelectedRadio(int valor) async{
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('genero', valor);
+
+    _genero = valor;
+    setState(() {});
   }
 
 
@@ -51,21 +70,13 @@ class _SettingsPageState extends State<SettingsPage> {
             value: 1,
             title: Text('Masculino'),
             groupValue: _genero,
-            onChanged: (value){
-              setState(() {
-                _genero = value;
-              });
-            },
+            onChanged: _setSelectedRadio,
           ),
           RadioListTile(
             value: 2,
             title: Text('Femenino'),
             groupValue: _genero,
-            onChanged: (value){
-              setState(() {
-                _genero = value;
-              });
-            },
+            onChanged: _setSelectedRadio,
           ),
           Divider(),
           Container(
