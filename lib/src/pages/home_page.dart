e8ddb4f17d0c52e218014a07e7a562fc950791cb
Efrain May <efrain_may_75@hotmail.com>
2021-01-14 01:07:50 -0600

    initial commit - preferencias de usuarios

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:preferenciasusuarioapp/src/pages/settings_page.dart';
+
+class HomePage extends StatelessWidget {
+
+  static final String routeName = 'home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Preferecias de Usuario'),
+      ),
+      drawer: _crearMenu(context),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Color secundario:'),
+          Divider(),
+          Text('Genero:'),
+          Divider(),
+          Text('Nombre de usuario:'),
+          Divider(),
+        ],
+      )
+    );
+  }
+
+  Drawer _crearMenu(BuildContext context) {
+
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Container(),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/menu-img.jpg'),
+                fit: BoxFit.cover
+              )
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.pages, color: Colors.blue),
+            title: Text('Home'),
+            onTap: () => Navigator.pushReplacementNamed(context, HomePage.routeName),
+          ),
+          ListTile(
+            leading: Icon(Icons.party_mode, color: Colors.blue),
+            title: Text('Party Mode'),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.people, color: Colors.blue),
+            title: Text('People'),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings, color: Colors.blue),
+            title: Text('Ajustes'),
+            onTap: () {
+              Navigator.pushReplacementNamed(context, SettingsPage.routeName);
+            }
+          ),
+        ],
+      ),
+    );
+  }
+}
